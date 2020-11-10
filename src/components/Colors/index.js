763052580918b081c6e189fedc7425d075273434
Luc <luc.georges.ld@gmail.com>
2020-11-10 16:19:05 +0100

    rework on color

--- src/components/Colors/index.js
+++ src/components/Colors/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-
 const Colors = ({direction, firstColor, lastColor, arrow}) => {
 
   if(direction == '90deg' ||direction =='45deg' ||direction =='135deg' ){
     return <div id="colors">
-      <span style={{ color: `${firstColor}` }}>{firstColor}</span>{arrow}
-      <span style={{ color: `${lastColor}` }}>{lastColor}</span>
+      <span style={{ color: `${firstColor}` }}>{firstColor} </span>{arrow}
+      <span style={{ color: `${lastColor}` }}>{lastColor}</span>   
     </div>;
     }else{
       return <div id="colors">
       <span style={{ color: `${lastColor}` }}>{lastColor}</span>{arrow}
       <span style={{ color: `${firstColor}` }}>{firstColor}</span>
     </div>;
-    }
-  ;
+    };
 
   }
 
